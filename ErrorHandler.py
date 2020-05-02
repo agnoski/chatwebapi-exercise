1bfaa44e11763a550f036f01aeb32cc15fa550ef
agno <crash.pc89@gmail.com>
2020-05-02 16:31:14 +0200

    Add error_handler decoratior and APIExceptions

---
+++ ErrorHandler.py
@@ -0,0 +1,18 @@
+class APIError(Exception):
+    statusCode = 500
+
+class Error404(APIError):
+    statusCode = 404
+
+def error_handler(fn):
+    def wrapper(*args,**kwargs):
+        try:
+            return fn(*args,**kwargs)
+        except APIError as e:
+            print(e)
+            return {
+                "status":"error",
+                "message":str(e)
+            }, e.statusCode
+    wrapper.__name__ = fn.__name__
+    return wrapper
