dcbe1eebad70f7450dc62dc101164006b49e3f86
agno <crash.pc89@gmail.com>
2020-05-03 23:51:27 +0200

    Remove unused Message class

--- Message.py
+++
@@ -1,7 +0,0 @@
-class Message:
-    def __init__(self, chat_code, user_code, user_name, line, text):
-        self.chat_code = chat_code
-        self.user_code = user_code
-        self.user_name = user_name
-        self.line = line
-        self.text = text
