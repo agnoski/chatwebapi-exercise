e0d5bf4658aabb84137e51dfd952f8a9de57ccf2
agno <crash.pc89@gmail.com>
2020-05-03 23:25:17 +0200

    Add BSONError exception \nAdd exception_info function

--- ErrorHandler.py
+++ ErrorHandler.py
@@ -1,18 +1,29 @@
+from bson.errors import BSONError
+
 class APIError(Exception):
     statusCode = 500
 
 class Error404(APIError):
     statusCode = 404
 
+def exception_info(status, message, error_code):
+    info = {
+        "status" : status,
+        "message" : message
+    }
+    return info, error_code
+
 def error_handler(fn):
-    def wrapper(*args,**kwargs):
+    def wrapper(*args, **kwargs):
         try:
-            return fn(*args,**kwargs)
+            return fn(*args, **kwargs)
         except APIError as e:
             print(e)
-            return {
-                "status":"error",
-                "message":str(e)
-            }, e.statusCode
+            info = exception_info('error', str(e), e.statusCode)
+            return info
+        except BSONError as e:
+            print(e)
+            info = exception_info('error', str(e), 500)
+            return info
     wrapper.__name__ = fn.__name__
     return wrapper
